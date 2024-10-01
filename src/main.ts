6f96b4a50dc0803acf0d7484822ce1e88bb372ca
patelkirtan03 <kirtan.patel@shaligraminfotech.com>
2024-10-01 12:54:27 +0530

    updated the main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-const google = require('./core/o-auths/google');
-const twitter = require('./core/o-auths/twitter');
+const google = require('./o-auths/google');
+const twitter = require('./o-auths/twitter');
 module.exports = {
   twitter,
   google
